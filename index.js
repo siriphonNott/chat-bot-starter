e27585fbe22cb0df78a3db3daab198daef68e4c3
siriphonnot <siriphonnot@gmail.com>
2018-12-11 05:28:45 +0700

    Change Port to 4000

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const message = {
   text: 'Hello World!'
 };
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 4000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -36,6 +36,10 @@ app.post('/webhook', (req, res) => {
             {
                 type: 'text',
                 text: 'Hello World!'
+            },
+            {
+                type: 'text',
+                text: 'https://devdocs.line.me/files/sticker_list.pdf'
             },
             {
                 type: "sticker",
