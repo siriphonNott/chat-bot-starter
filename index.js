2dea0941567d96f4a1ca26c6385de458a3b2f07d
siriphonnot <siriphonnot@gmail.com>
2018-12-11 05:20:38 +0700

    Add @line/bot-sdk

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+  channelAccessToken: 'FDY1JdEITEcFrbjgqdukNhM4/GxENlfiQEjvv8XBiUz829hhLNcOV9l2Elm8zidqhgiHKvogmp/jh1PKh3vLMmn8s5Evf6LChB93r1agMcg0qJh3Q1nb3SVeaJjY0fK7hDuuYjGr6Nb1fgEJdpV1xwdB04t89/1O/w1cDnyilFU='
+});
+
+const message = {
+  type: 'text',
+  text: 'Hello World!'
+};
+
 var PORT = process.env.PORT || 5000;
 
 // parse application/x-www-form-urlencoded
@@ -17,8 +28,32 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', (req, res) => {
     console.log('POST /webhook');
-    console.log(req.body);
-    res.send({status: '/webhook : ok'});
+    if(Object.keys(req.body).length !== 0) {
+        let events = req.body.events[0];
+        let replyToken = events.replyToken;
+
+        const message = [
+            {
+                type: 'text',
+                text: 'Hello World!'
+            },
+            {
+                type: "sticker",
+                packageId: "1",
+                stickerId: "1"
+            }
+        ];
+
+        client.replyMessage(replyToken, message)
+        .then(() => {
+            console.log('Reply is successfully!');
+        })
+        .catch((err) => {
+            console.log('Error: '+err);
+        });
+    } else {
+        console.log('No data');
+    }
 });
 
 app.listen(PORT, () => {
